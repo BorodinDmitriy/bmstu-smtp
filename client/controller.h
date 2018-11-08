570acecbe7614ad8e6e34ae49ebc9ac7f65d4cce
Pavel Fedorov <catkiller95@gmail.com>
2018-11-08 23:01:00 +0300

    Клиент: Изменения -> изменение в структуре контроллера

--- client/controller.h
+++ client/controller.h
@@ -1,22 +1,21 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/socket.h>
 #include <sys/select.h>
 #include <unistd.h>
+#include "structs.h"
 #include "smtp_socket.h"
 
 #define INIT_FD_SET_COUNT 10
 
 struct Controller
 {
-    int *fdSet;
-    int pointer;
-    int limit;
-    int maxFD;
+    struct FileDescSet readers;
+    struct FileDescSet writers;
+    struct FileDescSet handlers;
 };
 
 void InitController();
