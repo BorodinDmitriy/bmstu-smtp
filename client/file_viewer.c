cde8996dea9c4369d831893411da7870b38660a9
Pavel Fedorov <catkiller95@gmail.com>
2018-11-09 01:58:43 +0300

    Заглушка для ReadDataFromFile

--- client/file_viewer.c
+++ client/file_viewer.c
@@ -1,5 +1,18 @@
 #include "file_viewer.h"
 
-void InitFileViewer() {
+void InitFileViewer() 
+{
 
+}
+
+struct Mail ReadDataFromFile(int fd) 
+{
+    //  fake
+    struct Mail letter = { (char *)calloc(100, sizeof(char)), 100, (char *)calloc(100, sizeof(char)), 100, (char *)calloc(100, sizeof(char)), 100};
+
+    letter.from = "IU7.2@yandex.ru";
+    letter.to = "IU7.2@yandex.ru";
+    letter.message = "Test SMTP";
+
+    return letter;
 }
