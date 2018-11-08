5068437bd0dafc6707c73d22a5d87a5cb642a91a
Pavel Fedorov <catkiller95@gmail.com>
2018-11-08 23:03:31 +0300

    Клиент: Изменения -> внесены в инициализацию в контроллера

--- client/controller.c
+++ client/controller.c
@@ -17,25 +17,54 @@ void allocateMemory(int);
 void InitController()
 {
     allocateMemory(INIT_FD_SET_COUNT);
-    Manager.pointer = 0;
 
-    InitSmtpSocket(& Manager.fdSet, & Manager.pointer, Manager.limit);
-    InitFileViewer();
+    FD_ZERO(&Manager.writers.set);
+    Manager.writers.count = 0;
+
+    FD_ZERO(&Manager.readers.set);
+    Manager.readers.count = 0;
+
+    InitSmtpSocket(& Manager.writers);
+    InitFileViewer(& Manager.readers);
     
     int readyFD = -1;
-    fd_set readSet;
-
-    FD_ZERO(&readSet);
-    for (int i = 0; i < Manager.pointer; i++) 
-    {
-        FD_SET(Manager.fdSet[i], &readSet);
-    }
-
-
+    struct timespec timer;
+    timer.tv_sec = 10;
+    timer.tv_nsec = 0;
     
     while (1)
     {
-        readyFD = pselect(Manager.pointer + 1, );
+        int n = Manager.writers.count + Manager.readers.count + Manager.handlers.count + 1;
+        readyFD = pselect(n, Manager.readers.set, Manager.writers.set, timer, NULL);
+
+        if (readyFD < 0) 
+        {
+            continue;
+        }
+
+        printf("ready new data\n");
+
+        if (FD_ISSET(readyFD, &Manager.readers.set)) 
+        {
+            //  client ready to read from file
+
+            continue;
+        }
+
+        if (FD_ISSET(readyFD, &Manager.writers.set)) 
+        {
+            //  client ready to send data 
+
+            continue;
+        }
+
+        if (FD_ISSET(readyFD, &Manager.handlers.set)) 
+        {
+            //  client receive interupt
+
+            break;
+        }
+        
     }
 }
 
@@ -45,13 +74,13 @@ void InitController()
 
 void allocateMemory(int count)
 {
-    Manager.fdSet = (int *)realloc(Manager.fdSet, sizeof(int) * count);
-    if (Manager.fdSet == NULL)
-    {
-        printf("Failure of allocate memory for 'fdSet'");
-        exit(-1);
-    }
+//     Manager.fdSet = (int *)realloc(Manager.fdSet, sizeof(int) * count);
+//     if (Manager.fdSet == NULL)
+//     {
+//         printf("Failure of allocate memory for 'fdSet'");
+//         exit(-1);
+//     }
 
-    Manager.limit = count;
+//     Manager.limit = count;
     return;
 }
