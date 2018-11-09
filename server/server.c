995b38f4874b0be5695065c1e52fd3f15e0fc19b
Borodin Dmitriy <demansen@yandex.ru>
2018-11-09 06:04:03 +0300

    smtp server is now listening on infinite loop

--- server/server.c
+++ server/server.c
@@ -4,82 +4,68 @@
 
 int main(int argc, char **argv) {
 
-	int server_fd;								// файловый дескриптор для серверного сокета
 	int new_socket;								// файловый дескриптор сокета, соединяющегося с сервером
-	// int read_bytes;								// число считанных байт
+	
 
 	struct sockaddr_in address;					// структура для обработки интернет-адресов
-	int opt = 1;
 	size_t addrlen = sizeof(address);
 
-	struct fd_linked_list *sockets;
+	struct fd_linked_list *sockets;				// список всех возможных локальных сокетов
+	int max_fd_value = -1; 						// максимальное значение файлового дескриптора для select()
+	struct fd_linked_list *p;	
+
 	sockets = init_sockets();
 	printf("%p\n",sockets);
-	exit(SERVER_EXIT_FAILURE);
-	//char buffer[SERVER_BUFFER_SIZE];			// принимающий буфер
 
-	//char *stub_message = "Hello from server";   // серверная заглушка-ответ
-
-	// создание файлового дескриптора серверного сокета
-	/*server_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_fd == 0) {
-		perror("socket() failed"); 
-        exit(SERVER_EXIT_FAILURE);
+	// узнаем значение файлового дескриптора для select()
+	for (p = sockets; p != NULL; p = p->next) {
+		(p->fd > max_fd_value) ? (max_fd_value = p->fd) : 1;
 	}
 
-	// принудительное связывание сокета с портом (8080)
-	// установка опций
-	// SOL_SOCKET - 
-	// SO_REUSEADDR - 
-	// SO_REUSEPORT - 
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT | SO_REUSEADDR, &opt, sizeof(opt))) {
-		perror("setsockopt() failed"); 
-        exit(SERVER_EXIT_FAILURE);
-	};
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons(SERVER_PORT);
+	// вечно слушающий цикл в поисках новых соединений
+	// и создающий по процессу на каждое соединение
+	while (1) {
+		fd_set socket_set;
+		FD_ZERO(&socket_set);
 
-	// непосредственное связывание сокета с портом
-	if (bind(server_fd, (struct sockaddr *) &address, sizeof(address)) < 0) 
-    { 
-        perror("bind() failed"); 
-        exit(SERVER_EXIT_FAILURE); 
-    } 
+		for (p = sockets; p != NULL; p = p->next) {
+			FD_SET(p->fd, &socket_set);
+		}
 
-    // говорим, что "слушаем" соединения на связанном порту 
-    if (listen(server_fd, SERVER_BACKLOG_SIZE) < 0) 
-    { 
-        perror("listen() failed"); 
-        exit(SERVER_EXIT_FAILURE); 
-    } */
+		// вечное ожидание соединения по одному из связанных сокетов
+		select(max_fd_value + 1, &socket_set, NULL, NULL, NULL);
 
-    // принимаем соединение
-    new_socket = accept(server_fd, (struct sockaddr *) &address,  (socklen_t*) &addrlen);
-    if (new_socket < 0) 
-    { 
-        perror("accept() failed"); 
-        exit(SERVER_EXIT_FAILURE); 
-    } 
+		// проходим по списку сокетов в поисках установленного соединения
+		for (p = sockets; p != NULL; p = p->next) {
+			if (FD_ISSET(p->fd, &socket_set)) {
+				// принимаем соединение
+    			new_socket = accept(p->fd, (struct sockaddr *) &address,  (socklen_t*) &addrlen);
+    			if (new_socket < 0) 
+    			{ 
+        			printf("accept() failed"); 
+        			continue;
+    			} 
 
-    // соединение принято - можно делать обработку smtp
-    int *sock_fd = (int *) malloc(sizeof(int));
-    *sock_fd = new_socket;
+    			// соединение принято - можно делать обработку smtp
+    			int *sock_fd = (int *) malloc(sizeof(int));
+    			*sock_fd = new_socket;
 
-    // создание дочернего процесса, где будет происходить обработка
-    pid_t pid;
-    switch (pid = fork()) {
-    	case -1:
-    		perror("accept() failed"); 
-        	exit(SERVER_EXIT_FAILURE); 
-        case 0:  // процесс - потомок
-        	printf("child process forked with pid: %d\n", getpid());
-        	printf("parent pid: %d\n", getppid());
-        	smtp_handler(sock_fd);
-        default: // процесс - родитель
-        	printf("here should be continue statement later\n");
-    }
-    
+    			// создание дочернего процесса, где будет происходить обработка
+    			pid_t pid;
+    			switch (pid = fork()) {
+    				case -1:
+    					perror("accept() failed"); 
+        				continue;
+        			case 0:  // процесс - потомок
+        				printf("child process forked with pid: %d\n", getpid());
+        				printf("parent pid: %d\n", getppid());
+        				smtp_handler(sock_fd);
+        			default: // процесс - родитель
+        				continue;
+    			}
+			}
+		}
+	}
 
 	return 0;
 }
