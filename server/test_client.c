8a47570ae4c26e0c878f2d53862e9d74ea2c45d0
Borodin Dmitriy <demansen@yandex.ru>
2018-11-16 03:40:35 +0300

    rewritten test client with select

--- server/test_client.c
+++ server/test_client.c
@@ -21,6 +21,8 @@ int main(int argc, char **argv) {
     //char *client_stub = "Hello from client"; 
     char helo_stub[] = "HELO mysmtpserver.com\r\n" \
     					"QUIT mysmtpserver.com\r\n" ; 
+
+    //char helo_stub[] = "Hello from client"; 
     
     // создаем файловый дескриптор сокета
     client_sock_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -29,6 +31,19 @@ int main(int argc, char **argv) {
         perror("socket() failed"); 
         exit(TEST_CLIENT_EXIT_FAILURE); 
     } 
+
+    int file_flags = fcntl(client_sock_fd, F_GETFL, 0);
+    if (file_flags == -1)
+    {
+        printf("Fail to receive socket flags");
+        exit(TEST_CLIENT_EXIT_FAILURE);
+    }
+
+    if (fcntl(client_sock_fd, F_SETFL, file_flags | O_NONBLOCK))
+    {	
+        printf("Fail to set flag 'O_NONBLOCK' for socket");
+        exit(TEST_CLIENT_EXIT_FAILURE); 
+    }
    
     // инициализация структуры адреса для подключения к серверу по известному порту
     memset(&serv_addr, '0', sizeof(serv_addr)); 
@@ -43,34 +58,96 @@ int main(int argc, char **argv) {
         exit(TEST_CLIENT_EXIT_FAILURE); 
     } 
    
-    // установка соединения с сервером
-    if (connect(client_sock_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
-    { 
-        perror("connect() failed"); 
-        exit(TEST_CLIENT_EXIT_FAILURE);
-    } 
+    fd_set socket_set;
+	FD_ZERO(&socket_set);
+	FD_SET(client_sock_fd, &socket_set);
 
-    // установили соединение - можно общаться с помощью send/recv
-    send(client_sock_fd, helo_stub, strlen(helo_stub), 0); 
-    //read_bytes = read(client_sock_fd, buffer, TEST_CLIENT_BUFFER_SIZE ); 
-    //printf("%s\n", buffer); 
+	// вечное ожидание соединения по одному из связанных сокетов
+	select(client_sock_fd + 1, &socket_set, NULL, NULL, NULL);
 
-    char receive_message[TEST_CLIENT_BUFFER_SIZE] = {};
-    while (1) {
-        int receive = recv(client_sock_fd, receive_message, TEST_CLIENT_BUFFER_SIZE, 0);
-        if (receive > 0) {
-            printf("%s", receive_message);
-            receive_message[3] = '\0';
-            if (STR_EQUAL(receive_message, "221")) {
-            	break;
-            }
-            memset(receive_message, 0, TEST_CLIENT_BUFFER_SIZE * sizeof(char));
-        } else if (receive == 0) {
-            break;
-        } else { 
-            break;
-        }
-    }
+	// если так и не дождались возможности писать - ошибка
+	if (!FD_ISSET(client_sock_fd, &socket_set)) {
+		printf("socket is not available for 15s\n");
+		exit(TEST_CLIENT_EXIT_FAILURE);
+	} else {
+		// установка соединения с сервером
+    	if (connect(client_sock_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+    	{ 
+        	perror("connect() failed"); 
+        	exit(TEST_CLIENT_EXIT_FAILURE);
+    	} 
+
+
+    	FD_ZERO(&socket_set);
+		FD_SET(client_sock_fd, &socket_set);
+
+		// вечное ожидание соединения по одному из связанных сокетов
+		select(client_sock_fd + 1, &socket_set, NULL, NULL, NULL);
+
+		// если так и не дождались возможности писать - ошибка
+		if (!FD_ISSET(client_sock_fd, &socket_set)) {
+			printf("socket is not available for 15s\n");
+			exit(TEST_CLIENT_EXIT_FAILURE);
+		} else {
+			// установили соединение - можно общаться с помощью send/recv
+    		send(client_sock_fd, helo_stub, strlen(helo_stub), 0); 
+    		//read_bytes = read(client_sock_fd, buffer, TEST_CLIENT_BUFFER_SIZE ); 
+    		printf("au\n"); 
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+    	
+
+    	char receive_message[TEST_CLIENT_BUFFER_SIZE] = {};
+    	while (1) {
+    		FD_ZERO(&socket_set);
+			FD_SET(client_sock_fd, &socket_set);
+
+			// вечное ожидание соединения по одному из связанных сокетов
+			select(client_sock_fd + 1, &socket_set, NULL, NULL, NULL);
+
+			// если так и не дождались возможности писать - ошибка
+			if (!FD_ISSET(client_sock_fd, &socket_set)) {
+				printf("socket is not available for 15s\n");
+				exit(TEST_CLIENT_EXIT_FAILURE);
+			} else {
+				int receive = recv(client_sock_fd, receive_message, TEST_CLIENT_BUFFER_SIZE, 0);
+        		printf("%d\n",receive);
+       		 	if (receive > 0) {
+            		printf("ua%s", receive_message);
+            		receive_message[3] = '\0';
+            		if (STR_EQUAL(receive_message, "221")) {
+            			break;
+            		}
+            		memset(receive_message, 0, TEST_CLIENT_BUFFER_SIZE * sizeof(char));
+        		} else if (receive == 0) {
+        			printf("bla");
+            		break;
+        		} else { 
+        			printf("ba");
+            		break;
+        		}
+			}
+
+
+        	
+    	}
+	}
+
+    
+
+
+    
 
     return 0; 
 }
