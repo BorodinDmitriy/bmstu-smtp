c0bc9b0ec1d8b64f0005605aa6a8bec9f7c32afe
Pavel Fedorov <catkiller95@gmail.com>
2019-01-11 21:38:20 +0300

    Тестирование

--- client/tests/test_smtp.c
+++ client/tests/test_smtp.c
@@ -7,71 +7,380 @@
 
 #include "../smtp_interface.h"
 
-TEST_FUNCT(domain_is_NULL) {
+TEST_FUNCT(domain_is_NULL)
+{
 	struct FileDesc *connection = (struct FileDesc *)malloc(sizeof(struct FileDesc));
 	CU_ASSERT_PTR_NOT_EQUAL(connection, NULL);
 	connection->domain = NULL;
-	
+
 	connection->prev_state = NULL_POINTER;
 	connection->current_state = PREPARE_SOCKET_CONNECTION;
 
 	int state = SMTP_Control(connection);
 
 	CU_ASSERT_NOT_EQUAL(state, 0);
-	CU_ASSERT_EQUAL(state,-2);
+	CU_ASSERT_EQUAL(state, -2);
 	free(connection);
 }
 
-// TEST_FUNCT(create_client_socket_has_message) {
-// 	struct client_socket *c_sock = (struct client_socket *)malloc(sizeof(struct client_socket));
-// 	int fd = 0;
-// 	int buffer_size = 2048;
-// 	int state = SOCKET_STATE_INIT;
-// 	int max_recepients = 10;
-// 	int needs_message = 1;
+TEST_FUNCT(bad_domain)
+{
+	struct FileDesc *connection = (struct FileDesc *)malloc(sizeof(struct FileDesc));
+	CU_ASSERT_PTR_NOT_EQUAL(connection, NULL);
+	connection->domain = NULL;
+	connection->domain = (char *)calloc(10, sizeof(char));
+	CU_ASSERT_PTR_NOT_EQUAL(connection->domain, NULL);
+	memset(connection->domain, '\0', 10);
+	strcpy(connection->domain, "bad-dom");
 
-// 	*c_sock = init_client_socket(fd, buffer_size, state, max_recepients, needs_message);
+	connection->prev_state = NULL_POINTER;
+	connection->current_state = PREPARE_SOCKET_CONNECTION;
 
-// 	CU_ASSERT_EQUAL(c_sock->fd,0);
-// 	CU_ASSERT_EQUAL(sizeof(c_sock->buffer),8);
-// 	CU_ASSERT_EQUAL(c_sock->state,SOCKET_STATE_INIT);
-// 	CU_ASSERT_PTR_NOT_EQUAL(c_sock->message,NULL);
-// 	CU_ASSERT_EQUAL(c_sock->message->recepients_num,0);
-// 	free(c_sock);
-// }
+	int state = SMTP_Control(connection);
 
-// TEST_FUNCT(handle_helo_state_init) {
-// 	char *msg_buffer = "HELO myserver.com";
-// 	char buffer_output[SERVER_BUFFER_SIZE];
-// 	struct client_socket *c_sock = (struct client_socket *)malloc(sizeof(struct client_socket));
-// 	int fd = 0;
+	CU_ASSERT_NOT_EQUAL(state, 0);
+	CU_ASSERT_EQUAL(state, -2);
+	free(connection->domain);
+	free(connection);
+}
 
-// 	int buffer_size = 2048;
-// 	int state = SOCKET_STATE_INIT;
-// 	int max_recepients = 10;
-// 	int needs_message = 1;
+TEST_FUNCT(connection_is_blocked)
+{
+	struct FileDesc *connection = (struct FileDesc *)malloc(sizeof(struct FileDesc));
+	CU_ASSERT_PTR_NOT_EQUAL(connection, NULL);
+	connection->domain = NULL;
+	connection->domain = (char *)calloc(10, sizeof(char));
+	CU_ASSERT_PTR_NOT_EQUAL(connection->domain, NULL);
+	memset(connection->domain, '\0', 10);
+	strcpy(connection->domain, "mail.ru");
 
-// 	*c_sock = init_client_socket(fd, buffer_size, state, max_recepients, needs_message);
+	connection->prev_state = NULL_POINTER;
+	connection->current_state = PREPARE_SOCKET_CONNECTION;
 
-// 	struct sockaddr_in sock_address;
+	int state = SMTP_Control(connection);
 
-//     sock_address.sin_family = AF_INET;
-//     sock_address.sin_addr.s_addr = INADDR_ANY;
-//     sock_address.sin_port = htons( SERVER_PORT );
+	CU_ASSERT_NOT_EQUAL(state, 0);
+	CU_ASSERT_EQUAL(state, 1);
+	free(connection->domain);
+	free(connection);
+}
 
-// 	int err = handle_HELO(c_sock, msg_buffer, buffer_output, &sock_address) ;
-// 	CU_ASSERT_EQUAL(c_sock->fd,0);
+TEST_FUNCT(greeting_bad_graph_state)
+{
+	struct FileDesc *connection = (struct FileDesc *)malloc(sizeof(struct FileDesc));
+	CU_ASSERT_PTR_NOT_EQUAL(connection, NULL);
+	connection->domain = NULL;
+	connection->domain = (char *)calloc(20, sizeof(char));
+	CU_ASSERT_PTR_NOT_EQUAL(connection->domain, NULL);
+	memset(connection->domain, '\0', 20);
+	strcpy(connection->domain, MY_DOMAIN);
 
-// 	CU_ASSERT_EQUAL(c_sock->state,SOCKET_STATE_WAIT);
-// 	CU_ASSERT_EQUAL(err,0);
-// 	free(c_sock);
-// }
+	connection->prev_state = NULL_POINTER;
+	connection->current_state = PREPARE_SOCKET_CONNECTION;
 
-int add_to_suit_smtp_test(CU_pSuite suite) {
-    if (suite) {
-        ADD_SUITE_TEST(suite, domain_is_NULL)
-        // ADD_SUITE_TEST(suite, create_client_socket_has_message)
-        // ADD_SUITE_TEST(suite, handle_helo_state_init)
-    }
-    return CU_get_error();
+	int state = SMTP_Control(connection);
+	connection->prev_state = SEND_QUIT;
+
+	state = SMTP_Control(connection);
+	CU_ASSERT_EQUAL(state, -1);
+	shutdown(connection->id, 0);
+	close(connection->id);
+	free(connection->domain);
+	free(connection);
+}
+
+TEST_FUNCT(in_greeting_not_correct_domain)
+{
+	struct FileDesc *connection = (struct FileDesc *)malloc(sizeof(struct FileDesc));
+	CU_ASSERT_PTR_NOT_EQUAL(connection, NULL);
+	connection->domain = NULL;
+	connection->domain = (char *)calloc(20, sizeof(char));
+	CU_ASSERT_PTR_NOT_EQUAL(connection->domain, NULL);
+	memset(connection->domain, '\0', 20);
+	strcpy(connection->domain, "samsung-np530u4c");
+
+	connection->prev_state = NULL_POINTER;
+	connection->current_state = PREPARE_SOCKET_CONNECTION;
+
+	//  connect
+	int state = SMTP_Control(connection);
+
+	sleep(1);
+	strncpy(connection->domain, "12345\0", 6);
+	state = SMTP_Control(connection);
+	CU_ASSERT_EQUAL(state, -4);
+
+	shutdown(connection->id, 0);
+	close(connection->id);
+	free(connection->domain);
+	free(connection);
+}
+
+TEST_FUNCT(in_greeting_421_status)
+{
+	struct FileDesc *connection = (struct FileDesc *)malloc(sizeof(struct FileDesc));
+	CU_ASSERT_PTR_NOT_EQUAL(connection, NULL);
+	connection->domain = NULL;
+	connection->domain = (char *)calloc(20, sizeof(char));
+	CU_ASSERT_PTR_NOT_EQUAL(connection->domain, NULL);
+	memset(connection->domain, '\0', 20);
+	strcpy(connection->domain, "samsung-np530u4c");
+
+	connection->prev_state = NULL_POINTER;
+	connection->current_state = PREPARE_SOCKET_CONNECTION;
+
+	//  connect
+	int state = SMTP_Control(connection);
+
+	sleep(60);
+	state = SMTP_Control(connection);
+	printf("%d\n", state);
+	shutdown(connection->id, 0);
+	close(connection->id);
+	free(connection->domain);
+	free(connection);
+	CU_ASSERT_EQUAL(state, -3);
+}
+
+TEST_FUNCT(send_ehlo_and_correct_result)
+{
+	struct FileDesc *connection = (struct FileDesc *)malloc(sizeof(struct FileDesc));
+	CU_ASSERT_PTR_NOT_EQUAL(connection, NULL);
+	connection->domain = NULL;
+	connection->domain = (char *)calloc(20, sizeof(char));
+	CU_ASSERT_PTR_NOT_EQUAL(connection->domain, NULL);
+	memset(connection->domain, '\0', 20);
+	strcpy(connection->domain, "samsung-np530u4c");
+	fd_set temp;
+	FD_ZERO(&temp);
+
+	connection->prev_state = NULL_POINTER;
+	connection->current_state = PREPARE_SOCKET_CONNECTION;
+
+	//  connect
+	int state = SMTP_Control(connection);
+	if (state == 1)
+		sleep(1);
+
+	//  receive GREETING
+	state = SMTP_Control(connection);
+
+	if (state == 1)
+		sleep(1);
+
+	//  send Ehlo
+	state = SMTP_Control(connection);
+	if (state == 1)
+		sleep(2);
+
+	//  response of Ehlo
+	state = SMTP_Control(connection);
+
+	//  blocked is ok
+	if (state == 1)
+	{
+		state = 0;
+	}
+
+	shutdown(connection->id, 0);
+	close(connection->id);
+	free(connection->domain);
+	free(connection);
+	CU_ASSERT_EQUAL(state, 0);
+}
+
+TEST_FUNCT(send_ehlo_and_change_state_on_error)
+{
+	InitDictionary();
+	struct FileDesc *connection = (struct FileDesc *)malloc(sizeof(struct FileDesc));
+	CU_ASSERT_PTR_NOT_EQUAL(connection, NULL);
+	connection->domain = NULL;
+	connection->domain = (char *)calloc(20, sizeof(char));
+	CU_ASSERT_PTR_NOT_EQUAL(connection->domain, NULL);
+	memset(connection->domain, '\0', 20);
+	strcpy(connection->domain, "samsung-np530u4c");
+	connection->task_pool = NULL;
+	fd_set temp;
+	FD_ZERO(&temp);
+
+	connection->prev_state = NULL_POINTER;
+	connection->current_state = PREPARE_SOCKET_CONNECTION;
+
+	//  connect
+	int state = SMTP_Control(connection);
+	if (state == 1)
+	{
+		FD_SET(connection->id, &temp);
+		pselect(connection->id + 1, &temp, &temp, NULL, NULL, NULL);
+		FD_ZERO(&temp);
+	}
+
+	state = 0;
+	while (state == 0)
+	{
+		//  receive GREETING
+		state = SMTP_Control(connection);
+		if (state == 0)
+			break;
+		FD_SET(connection->id, &temp);
+		pselect(connection->id + 1, &temp, NULL, NULL, NULL, NULL);
+		FD_ZERO(&temp);
+	}
+
+	state = 1;
+	//  send Ehlo
+	while (state == 1)
+	{
+		state = SMTP_Control(connection);
+		if (state == 0)
+			break;
+		FD_SET(connection->id, &temp);
+		pselect(connection->id + 1, NULL, &temp, NULL, NULL, NULL);
+		FD_ZERO(&temp);
+	}
+
+	connection->current_state = SMTP_ERROR;
+	//  resolve error
+	state = SMTP_Control(connection);
+
+	CU_ASSERT_EQUAL(connection->current_state, DISPOSING_SOCKET);
+	//  Disposing
+	state = SMTP_Control(connection);
+
+	shutdown(connection->id, 0);
+	close(connection->id);
+	free(connection->domain);
+	free(connection);
+	FreeDictionary();
+	CU_ASSERT_EQUAL(state, 0);
+}
+
+TEST_FUNCT(send_and_recv_ehlo_send_and_recv_mail_from_correct)
+{
+	InitDictionary();
+	struct FileDesc *connection = (struct FileDesc *)malloc(sizeof(struct FileDesc));
+	CU_ASSERT_PTR_NOT_EQUAL(connection, NULL);
+	connection->domain = NULL;
+	connection->domain = (char *)calloc(20, sizeof(char));
+	CU_ASSERT_PTR_NOT_EQUAL(connection->domain, NULL);
+	memset(connection->domain, '\0', 20);
+	strcpy(connection->domain, "samsung-np530u4c");
+	connection->task_pool = NULL;
+	connection->task_pool = (struct worker_task *)malloc(sizeof(struct worker_task));
+	connection->task_pool->domain = NULL;
+	connection->task_pool->next=NULL;
+	connection->task_pool->path = NULL;
+	connection->task_pool->domain = (char *)calloc(20, sizeof(char));
+	memset(connection->task_pool->domain, '\0', 20);
+	strcpy(connection->task_pool->domain, connection->domain);
+	connection->task_pool->path = (char *)calloc(20, sizeof(char));
+	memset(connection->task_pool->path, '\0', 20);
+	strcpy(connection->task_pool->path, "fakemail.eml");
+	fd_set temp;
+	FD_ZERO(&temp);
+
+	connection->prev_state = NULL_POINTER;
+	connection->current_state = PREPARE_SOCKET_CONNECTION;
+
+	//  connect
+	int state = SMTP_Control(connection);
+	if (state == 1)
+	{
+		FD_SET(connection->id, &temp);
+		pselect(connection->id + 1, &temp, &temp, NULL, NULL, NULL);
+		FD_ZERO(&temp);
+	}
+
+	//  receive GREETING
+	state = 1;
+	while (state == 1)
+	{	
+		state = SMTP_Control(connection);
+		if (state == 0)
+			break;
+		FD_SET(connection->id, &temp);
+		pselect(connection->id + 1, &temp, NULL, NULL, NULL, NULL);
+		FD_ZERO(&temp);
+	}
+
+	
+	//  send Ehlo
+	state = 1;
+	while (state == 1)
+	{
+		state = SMTP_Control(connection);
+		if (state == 0)
+			break;
+		FD_SET(connection->id, &temp);
+		pselect(connection->id + 1, NULL, &temp, NULL, NULL, NULL);
+		FD_ZERO(&temp);
+	}
+
+	//  receive Response
+	state = 1;
+	while (state == 1)
+	{
+		state = SMTP_Control(connection);
+		if (state == 0)
+			break;
+		FD_SET(connection->id, &temp);
+		pselect(connection->id + 1, &temp, NULL, NULL, NULL, NULL);
+		FD_ZERO(&temp);
+	}
+
+	CU_ASSERT_EQUAL(connection->current_state, SEND_MAIL_FROM);
+	//  send MAIL FROM
+	state = 1;
+	while (state == 1)
+	{
+		state = SMTP_Control(connection);
+		if (state == 0)
+			break;
+		FD_SET(connection->id, &temp);
+		pselect(connection->id + 1, NULL, &temp, NULL, NULL, NULL);
+		FD_ZERO(&temp);
+	}
+
+	CU_ASSERT_EQUAL(connection->current_state, RECEIVE_MAIL_FROM_RESPONSE);
+	//  receive MAIL FROM
+	state = 1;
+	while (state == 1)
+	{
+		state = SMTP_Control(connection);
+		if (state == 0)
+			break;
+		FD_SET(connection->id, &temp);
+		pselect(connection->id + 1, &temp, NULL, NULL, NULL, NULL);
+		FD_ZERO(&temp);
+	}
+	CU_ASSERT_EQUAL(connection->current_state, SEND_RCPT_TO);
+
+	//free
+
+	shutdown(connection->id, 0);
+	close(connection->id);
+	free(connection->task_pool->path);
+	free(connection->task_pool->domain);
+	free(connection->task_pool);
+	free(connection->domain);
+	free(connection);
+	FreeDictionary();
+	CU_ASSERT_EQUAL(state, 0);
+}
+
+int add_to_suit_smtp_test(CU_pSuite suite)
+{
+	if (suite)
+	{
+		ADD_SUITE_TEST(suite, domain_is_NULL)
+		ADD_SUITE_TEST(suite, bad_domain)
+		ADD_SUITE_TEST(suite, connection_is_blocked)
+		ADD_SUITE_TEST(suite, greeting_bad_graph_state)
+		ADD_SUITE_TEST(suite, in_greeting_not_correct_domain)
+		ADD_SUITE_TEST(suite, send_ehlo_and_correct_result)
+		ADD_SUITE_TEST(suite, send_ehlo_and_change_state_on_error)
+		// ADD_SUITE_TEST(suite, send_and_recv_ehlo_in_receive_not_my_domain)
+		// ADD_SUITE_TEST(suite, in_greeting_421_status)
+	}
+	return CU_get_error();
 }
--- client/tests/test_smtp.h
+++ client/tests/test_smtp.h
@@ -3,6 +3,17 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <signal.h>
+#include <err.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/file.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/select.h>
 #include <CUnit/Basic.h>
 #include "utils.h"
 #include "../smtp_interface.h"
