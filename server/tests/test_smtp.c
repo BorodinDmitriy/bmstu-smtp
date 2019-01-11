385c0541b45495a9ba904df6bc7a4c998eaad929
Borodin Dmitriy <demansen@yandex.ru>
2019-01-11 15:54:45 +0300

    added client_socket creation test with message needed

--- server/tests/test_smtp.c
+++ server/tests/test_smtp.c
@@ -24,10 +24,28 @@ TEST_FUNCT(create_client_socket_no_message) {
 	free(c_sock);
 }
 
+TEST_FUNCT(create_client_socket_has_message) {
+	struct client_socket *c_sock = (struct client_socket *)malloc(sizeof(struct client_socket));
+	int fd = 0;
+	int buffer_size = 2048;
+	int state = SOCKET_STATE_INIT;
+	int max_recepients = 10;
+	int needs_message = 1;
+
+	*c_sock = init_client_socket(fd, buffer_size, state, max_recepients, needs_message);
+
+	CU_ASSERT_EQUAL(c_sock->fd,0);
+	CU_ASSERT_EQUAL(sizeof(c_sock->buffer),8);
+	CU_ASSERT_EQUAL(c_sock->state,SOCKET_STATE_INIT);
+	CU_ASSERT_PTR_NOT_EQUAL(c_sock->message,NULL);
+	CU_ASSERT_EQUAL(c_sock->message->recepients_num,0);
+	free(c_sock);
+}
+
 int add_to_suit_smtp_test(CU_pSuite suite) {
     if (suite) {
         ADD_SUITE_TEST(suite, create_client_socket_no_message)
+        ADD_SUITE_TEST(suite, create_client_socket_has_message)
     }
-    printf("BLA\n");
     return CU_get_error();
 }
